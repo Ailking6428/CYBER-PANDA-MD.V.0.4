b39a39bdf04676e93ae2d36165f18835b7ab82b0
Ailking6428 <162872704+Ailking6428@users.noreply.github.com>
2024-04-24 12:36:55 +0500

    Update config.js

--- config.js
+++ config.js
@@ -21,13 +21,13 @@ global.prefa = ["."];                                    //Default prefix here. 
 
 
 //
-global.Owner = ["+94772108460", "+94772108460"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["+94772108460", "+94772108460"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.Owner = ["+94772108460", "+94772108460"];         //If you want singal number so global.Owner = ['61488878858'] Change into your number.
+global.OwnerNumber = ["+94772108460", "+94772108460"];   //If you want singal number so global.Owner = ['61488878858'] Change into your number.
 global.ownertag = ["+94772108460"];
 global.OwnerName = "KUSHAN";
 global.BotName = "DARK SHAM";
-global.packname = "🦋 DARK SHAN 🦋";                             //Do not change.
-global.author = "KUSHAN";                               //Do not change.
+global.packname = "│𝐔𝐌𝐀𝐑│𝐊𝐀𝐇𝐍│🦋";                             //Do not change.
+global.author = "•⏤⵿༄ 𝆺𝅥⃝⃪⃜❤️𝐔𝐌𝐀𝐑🦋𓆪ꪾ❦︎ 𝆺𝅥⃝🍷";                               //Do not change.
 global.BotSourceCode = "https://github.com/Kai00"; //Do not change.
 global.SupportGroupLink = "https://chat.whatsapp.com/GhRGdwfaMVDCoeAdzILfl";
 
